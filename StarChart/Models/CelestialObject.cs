4cedb7ca0883d43b3addef8c3fb489d794b3e907
Simon Tournay <simontournay@gmail.com>
2020-03-18 14:23:00 +0000

    fixed property name

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -16,7 +16,7 @@ namespace StarChart.Models
 
         public int? OrbitedObjectId { get; set; }
         [NotMapped]
-        public List<CelestialObject> Satelites { get; set; }
+        public List<CelestialObject> Satellites { get; set; }
 
         public TimeSpan OrbitalPeriod { get; set; }
     }
